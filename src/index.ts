7901aac2027f63d8cee47a02d0d3a87ba87963eb
Ashish Nitro <ashishnitro5@gmail.com>
2025-03-14 11:40:30 +0530

    api end points

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-function greet(name: string): string {
-    return `Hello From Ashish To try , ${name}! Welcome to coreIdea.`;
-  }
-  
-  console.log(greet("Developer"));
+import jwt from "jsonwebtoken";
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+
+app.post("/api/v1/signup", (req, res) =>{
+
+})
+
+app.post("/api/v1/signin", (req, res) =>{
+    
+})
+
+app.post("/api/v1/content", (req, res) =>{
+    
+})
+
+app.get("/api/v1/content", (req, res) =>{
+    
+})
+
+app.delete("/api/v1/content", (req, res) =>{
+    
+})
