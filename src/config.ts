bcd4bb466f877f7446e470285b83bf48ab7b22da
Ashish Nitro <ashishnitro5@gmail.com>
2025-03-15 06:31:57 +0530

    content updated

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+// will later on store in .env file
+export const JWT_PASSWORD = "789456789456";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 import express from "express";
 import mongoose from "mongoose";
-import { UserModel } from "./db";
+import { ContentModel, UserModel } from "./db";
+import { JWT_PASSWORD } from "./config"
+import { useMiddleware } from "./middleware";
 
 
 const app = express();
@@ -31,20 +33,72 @@ app.post("/api/v1/signup", async (req, res) =>{
 
 
 app.post("/api/v1/signin", async (req, res) =>{
-      
+    const { username, password,} = req.body;
+    
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if(existingUser){
+        const token = jwt.sign({
+            id:existingUser._id
+        }, JWT_PASSWORD)
+        res.json({
+            token
+        })
+    } else{
+        res.status(403).json({
+            message: "Incorrect credentils"
+        })
+    }
+
+})
+
+app.post("/api/v1/content",  useMiddleware, async (req, res) =>{
+    const link = req.body.Link;
+    const type = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        title: req.body.title,
+        //@ts-ignore
+        userId: req.userId,
+        tags: []
+    })
+
+    res.json({
+        message: "Content Added"
+    })
+
+
+    
+})
+
+app.get("/api/v1/content",useMiddleware, async (req, res) =>{
+    //@ts-ignore
+   const userId = req.userId;
+    
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
+    
+})
+
+app.delete("/api/v1/content", async (req, res) =>{
+    const contentId =req.body.contentId;
      
-    
-})
+    await ContentModel.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId: req.userId
 
-app.post("/api/v1/content", (req, res) =>{
-    
-})
-
-app.get("/api/v1/content", (req, res) =>{
-    
-})
-
-app.delete("/api/v1/content", (req, res) =>{
+     })
+     res.json({
+        message: "Deleted"
+     })
     
 })
 
